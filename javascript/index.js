820a3d12acfc206c36cbeff1d634c57ccea0bb11
latif <latifjonsheraliyev209@gmail.com>
2024-09-16 07:50:14 +0500

    takrorlash

---
+++ javascript/index.js
@@ -0,0 +1,27 @@
+// 1-smiol 
+// function latif(arr) {
+//   let result = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     let yangi = false;
+//     for (let j = 0; j < result.length; j++) {
+//       if (arr[i] === result[j]) {
+//         yangi = true;
+//         break;
+//       }
+//     }
+//     if (!yangi) {
+//       result.push(arr[i]);
+//     }
+//   }
+//   return result;
+// }
+// console.log(latif([1, 2, 2, 3, 4, 5, 5]));
+
+
+// 2-msiol
+// function sortArray(arr) {
+//   return arr.sort((a, b) => a - b);
+// }
+
+// const result = sortArray([34, 23, 45, 62]);
+// console.log(result);
